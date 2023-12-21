cf1c9da979282106ac8fb3dfefa930702b6ee99f
Adam Solove <asolove@gmail.com>
2023-12-21 13:01:06 -0700

    Day 19, part 1

--- d19/index.ts
+++ d19/index.ts
@@ -1,4 +1,4 @@
-import { input } from "../lib";
+import { input, sum } from "../lib";
 import {
   Parser,
   alpha,
@@ -18,24 +18,29 @@ type Outcome = "R" | "A";
 
 let isFinal = (s: string): s is Outcome => s === "R" || s === "A";
 
-type Instruction = {
+type TestRule = {
+  type: "test";
   key: string;
-  op: "<" | ">";
-  val: number;
-  outcome: string;
+  value: number;
+  op: ">" | "<";
+  dest: string;
 };
-
-type Rule =
-  | { type: "test"; key: string; value: number; op: ">" | "<"; dest: string }
-  | { type: "jump"; dest: string };
+type JumpRule = { type: "jump"; dest: string };
+type Rule = TestRule | JumpRule;
 
 type Workflow = {
   name: string;
-  instructions: Rule[];
-  else: string; // name of a rule
+  rules: Rule[];
 };
 
+type Workflows = { [name: string]: Workflow };
+
 type Item = { x: number; m: number; a: number; s: number };
+
+type File = {
+  workflows: Workflows;
+  items: Item[];
+};
 
 let name = map(many1(alpha), (rs) => rs.join(""));
 let jumpRule: Parser<Rule> = map(name, (c) => ({
@@ -54,7 +59,10 @@ let testRule: Parser<Rule> = map(seq(test, seq(char(":"), name)), (r) => ({
 }));
 let rule = plus(testRule, jumpRule);
 let rules = surroundedBy(separatedBy(rule, char(",")), char("{"), char("}"));
-let workflow = seq(name, rules);
+let workflow: Parser<Workflow> = map(seq(name, rules), (r) => ({
+  name: r[0],
+  rules: r[1],
+}));
 let workflows = separatedBy(workflow, char("\n"));
 
 let entry = seq(alpha, seq(char("="), int));
@@ -64,14 +72,50 @@ let item = map(
 );
 let items = separatedBy(item, char("\n"));
 
-let file = map(seq(workflows, seq(string("\n\n"), items)), (r) => ({
-  rules: r[0],
-  items: r[1][1],
-}));
+let file: Parser<File> = map(
+  seq(workflows, seq(string("\n\n"), items)),
+  (r) => ({
+    workflows: r[0].reduce((r, w) => ({ ...r, [w.name]: w }), {}),
+    items: r[1][1],
+  })
+);
 
 let parsedFile = parse(file, await input());
 
-console.log(parsedFile);
+let passTestRule = (r: TestRule, i: Item): boolean =>
+  r.op === ">" ? i[r.key] > r.value : i[r.key] < r.value;
+
+let naiveEval = (item: Item, workflows: Workflows): Outcome => {
+  let goto = (name: string): Outcome => {
+    if (isFinal(name)) return name;
+    return evalWorkflow(workflows[name]);
+  };
+  let evalRule = (r: Rule, rs: Rule[]): Outcome => {
+    if (r.type === "jump") {
+      return goto(r.dest);
+    } else {
+      if (passTestRule(r, item)) {
+        return goto(r.dest);
+      } else {
+        let [r2, ...rest] = rs;
+        return evalRule(r2, rest);
+      }
+    }
+  };
+  let evalWorkflow = (w: Workflow): Outcome => {
+    return evalRule(w.rules[0], w.rules.slice(1));
+  };
+  return goto("in");
+};
+
+let itemScore = (i: Item): number => i.x + i.m + i.a + i.s;
+
+console.log(
+  parsedFile.items
+    .map((item) => naiveEval(item, parsedFile.workflows))
+    .map((r, i) => (r === "A" ? itemScore(parsedFile.items[i]) : 0))
+    .reduce(sum)
+);
 
 /* 
 Need it to be faster? 
